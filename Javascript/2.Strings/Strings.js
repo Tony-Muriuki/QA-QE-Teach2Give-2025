aad3cc0fb560c895270c8ca51f47b496c1bad284
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-09 21:18:02 +0300

    String to Array of Words

--- Javascript/2.Strings/Strings.js
+++ Javascript/2.Strings/Strings.js
@@ -10,10 +10,17 @@ function is_string(str) {
 console.log(is_string("w3resource"));
 console.log(is_string([1, 2, 4, 0]));
 
-/* FUNCTION to Check Blank String*/
+/*2: FUNCTION to Check Blank String*/
 function is_Blank(str) {
   let output = str === "" ? "Empty String" : "Not Empty string";
   return output;
 }
 console.log(is_Blank(""));
 console.log(is_Blank("abc"));
+
+/*3: String to Array of Words*/
+function string_to_array(str) {
+  let array_to_string = str.split(" ");
+  return array_to_string;
+}
+console.log(string_to_array("Robin Singh"));
