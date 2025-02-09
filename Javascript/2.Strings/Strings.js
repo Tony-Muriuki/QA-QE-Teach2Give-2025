5fa5f7ef4187bcf049b8b341e1eeac4630f4da01
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-09 20:54:26 +0300

    Check String Input

--- Javascript/2.Strings/Strings.js
+++ Javascript/2.Strings/Strings.js
@@ -1,1 +1,10 @@
 "strict";
+
+/*1: FUNCTION to check whether an 'input' is a STRING or NOT*/
+
+function is_string(str) {
+  let input_type = typeof str;
+  return input_type;
+}
+console.log(is_string("w3resource"));
+console.log(is_string([1, 2, 4, 0]));
