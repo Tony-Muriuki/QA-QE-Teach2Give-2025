eddca1fa6c96958939edc115531dc09d2894eb79
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-09 22:03:33 +0300

    Capitalize Each Word

--- Javascript/2.Strings/Strings.js
+++ Javascript/2.Strings/Strings.js
@@ -70,3 +70,13 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 console.log(capitalize("js string exercises")); // "Js string exercises"
+
+/*9: Capitalize Each Word*/
+
+function capitalize_Words(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+console.log(capitalize_Words("js string exercises")); // "Js String Exercises"
