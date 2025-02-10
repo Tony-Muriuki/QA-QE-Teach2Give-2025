a2ce0f98bfc7f44b88aca931dbe0bad5215ae4ef
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-10 14:31:23 +0300

    Remove Duplicates from a String

--- Javascript/4.JSassignment/Assignment.js
+++ Javascript/4.JSassignment/Assignment.js
@@ -7,3 +7,16 @@ function reverse_string(str) {
   return reversed_string;
 }
 console.log(reverse_string("Reverse"));
+
+/* Remove Duplicates from a String*/
+function remove_duplicate(str) {
+  let uniquestr = "";
+  for (let char of str) {
+    if (!uniquestr.includes(char)) {
+      uniquestr += char;
+    }
+  }
+  return uniquestr;
+}
+console.log(remove_duplicate("Programming"));
+console.log(remove_duplicate("Hello world"));
