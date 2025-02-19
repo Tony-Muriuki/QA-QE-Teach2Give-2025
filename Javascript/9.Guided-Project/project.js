d70b59fda2e7173eed63e19597313a700043751e
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-19 20:53:15 +0300

    dbJson

--- Javascript/9.Guided-Project/project.js
+++ Javascript/9.Guided-Project/project.js
@@ -1,1 +1,8 @@
 "use strict";
+
+async function fetchData(callback) {
+  const response = await fetch("http://localhost:3000/Books");
+  const data = await response.json();
+  console.log(data);
+  callback(data);
+}
