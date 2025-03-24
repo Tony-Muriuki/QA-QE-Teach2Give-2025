57eed2452bd0d28263a5ee633baa896cf9f8e250
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-03-24 14:55:35 +0300

    AsyncHandler

--- 4.Express/6.CRUD/3.SOC-SeparationOfConcern/src/middlewares/asyncHandler.ts
+++
@@ -1,2 +0,0 @@
-/*
-An async handler in middleware is a function that handles asynchronous operations inside an Express middleware. Since middleware functions often deal with tasks like database queries, API calls, or file operations, they need to handle asynchronous behavior properly.*/
