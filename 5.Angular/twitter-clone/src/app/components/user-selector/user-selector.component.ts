32d35d85a3c1f19ef9ee98c586e0c1c614fc8b4a
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-03-28 10:31:51 +0300

    User-Selector Component

---
+++ 5.Angular/twitter-clone/src/app/components/user-selector/user-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-selector',
+  imports: [],
+  templateUrl: './user-selector.component.html',
+  styleUrl: './user-selector.component.scss'
+})
+export class UserSelectorComponent {
+
+}
