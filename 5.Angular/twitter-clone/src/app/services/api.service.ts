668ce8e45dc3d2cd36ae32fa9f02bb336e344897
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-03-28 10:39:02 +0300

    Updated API Service

--- 5.Angular/twitter-clone/src/app/services/api.service.ts
+++ 5.Angular/twitter-clone/src/app/services/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.BASE_URL}/users`);
+  }
+
+  getPostsByUser(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.BASE_URL}/posts?userId=${userId}`);
+  }
+
+  getCommentsByPost(postId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.BASE_URL}/comments?postId=${postId}`);
+  }
 }
