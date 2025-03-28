ac1637483e7f0b848739982c14a74c878504a0d6
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-03-28 10:18:56 +0300

    Angulaer Tutorial

--- 5.Angular/angular19Tutorial/src/app/app.component.ts
+++ 5.Angular/angular19Tutorial/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
+// import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
+import { NgForComponent } from './components/ng-for/ng-for.component';
 
 @Component({
   selector: 'app-root',
-  imports: [DataBindingComponent],
+  imports: [NgIfComponent, NgForComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
