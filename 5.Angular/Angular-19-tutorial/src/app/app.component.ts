c93ca0c1516c976856ff8ee9290e7a83b811aa2f
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-04-10 01:21:05 +0300

    Directives

--- 5.Angular/Angular-19-tutorial/src/app/app.component.ts
+++ 5.Angular/Angular-19-tutorial/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DatabindingComponent } from './databinding/databinding.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DatabindingComponent],
+  imports: [RouterOutlet, DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ 5.Angular/Angular-19-tutorial/src/app/directives/directives.component.ts
@@ -0,0 +1,11 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { DatabindingComponent } from '../databinding/databinding.component';
+
+@Component({
+  selector: 'app-directives',
+  imports: [],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css',
+})
+export class DirectivesComponent {}
