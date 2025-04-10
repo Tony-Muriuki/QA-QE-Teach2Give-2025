b7849679356587bba374ec9e6408e1acdc6e39da
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-04-10 12:12:01 +0300

    Angular Crash

--- 5.Angular/Angular-19-tutorial/src/app/app.component.ts
+++ 5.Angular/Angular-19-tutorial/src/app/app.component.ts
@@ -3,10 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DatabindingComponent } from './databinding/databinding.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { StructuralDirectiveNgIfVsIfComponent } from './structural-directive-ng-if-vs-if/structural-directive-ng-if-vs-if.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DirectivesComponent],
+  imports: [
+    RouterOutlet,
+    // DirectivesComponent,
+    StructuralDirectiveNgIfVsIfComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ 5.Angular/Angular-19-tutorial/src/app/structural-directive-ng-if-vs-if/structural-directive-ng-if-vs-if.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-directive-ng-if-vs-if',
+  imports: [],
+  templateUrl: './structural-directive-ng-if-vs-if.component.html',
+  styleUrl: './structural-directive-ng-if-vs-if.component.css'
+})
+export class StructuralDirectiveNgIfVsIfComponent {
+
+}
